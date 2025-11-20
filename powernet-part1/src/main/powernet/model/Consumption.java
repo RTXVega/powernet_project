ceeca4859ab9d2db3e237e542e05ffd2dc391548
ani-bouhail <108761004+ani-bouhail@users.noreply.github.com>
2025-11-20 16:14:02 +0100

    Delete powernet-part1 directory

--- powernet-part1/src/main/powernet/model/Consumption.java
+++
@@ -1,9 +0,0 @@
-package powernet.model;
-
-/** Niveau de consommation d'une maison (kW). */
-public enum Consumption {
-    BASSE(10), NORMAL(20), FORTE(40);
-    private final int kw;
-    Consumption(int kw) { this.kw = kw; }
-    public int getKw() { return kw; }
-}
