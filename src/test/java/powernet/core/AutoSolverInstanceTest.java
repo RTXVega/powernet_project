f022e4bfac40a64a8b3adced1ace504d8a55ca68
Rui MA <dennismachn@outlook.com>
2025-12-15 22:55:34 +0100

    Added AutoSolverInstanceTest.java to automatically test the .txt files in instances/ and compare to expected results.

---
+++ src/test/java/powernet/core/AutoSolverInstanceTest.java
@@ -0,0 +1,121 @@
+package powernet.core;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AutoSolverInstanceTest {
+
+    private static final double EPS_1E3 = 1e-3; // comparaison à 3 décimales
+
+    @Test
+    @DisplayName("Test instance1.txt")
+    void autoSolverTestInstance1() throws Exception {
+        Path instancePath = Path.of("instances", "instance1.txt");
+        double expectedCost = 0.698;
+        double lambda = 10.0;
+
+        Network net = new NetworkParser().parse(instancePath);
+        CostCalculator calc = new CostCalculator(lambda);
+
+        new AutoSolver(1000).improve(net, calc);
+        double actualCost = calc.compute(net).total();
+
+        assertEquals(expectedCost, actualCost, EPS_1E3);
+    }
+
+    @Test
+    @DisplayName("Test instance2.txt")
+    void autoSolverTestInstance2() throws Exception {
+        Path instancePath = Path.of("instances", "instance2.txt");
+        double expectedCost = 1.059;
+        double lambda = 10.0;
+
+        Network net = new NetworkParser().parse(instancePath);
+        CostCalculator calc = new CostCalculator(lambda);
+
+        new AutoSolver(1000).improve(net, calc);
+        double actualCost = calc.compute(net).total();
+
+        assertEquals(expectedCost, actualCost, EPS_1E3);
+    }
+
+    @Test
+    @DisplayName("Test instance3.txt")
+    void autoSolverTestInstance3() throws Exception {
+        Path instancePath = Path.of("instances", "instance3.txt");
+        double expectedCost = 0;
+        double lambda = 10.0;
+
+        Network net = new NetworkParser().parse(instancePath);
+        CostCalculator calc = new CostCalculator(lambda);
+
+        new AutoSolver(1000).improve(net, calc);
+        double actualCost = calc.compute(net).total();
+
+        assertEquals(expectedCost, actualCost, EPS_1E3);
+    }
+
+    @Test
+    @DisplayName("Test instance4.txt")
+    void autoSolverTestInstance4() throws Exception {
+        Path instancePath = Path.of("instances", "instance4.txt");
+        double expectedCost = 0;
+        double lambda = 10.0;
+
+        Network net = new NetworkParser().parse(instancePath);
+        CostCalculator calc = new CostCalculator(lambda);
+
+        new AutoSolver(1000).improve(net, calc);
+        double actualCost = calc.compute(net).total();
+
+        assertEquals(expectedCost, actualCost, EPS_1E3);
+    }
+    @Test
+    @DisplayName("Test instance5.txt")
+    void autoSolverTestInstance5() throws Exception {
+        Path instancePath = Path.of("instances", "instance5.txt");
+        double expectedCost = 1.511;
+        double lambda = 10.0;
+
+        Network net = new NetworkParser().parse(instancePath);
+        CostCalculator calc = new CostCalculator(lambda);
+
+        new AutoSolver(1000).improve(net, calc);
+        double actualCost = calc.compute(net).total();
+
+        assertEquals(expectedCost, actualCost, EPS_1E3);
+    }
+    @Test
+    @DisplayName("Test instance6.txt")
+    void autoSolverTestInstance6() throws Exception {
+        Path instancePath = Path.of("instances", "instance6.txt");
+        double expectedCost = 0.755;
+        double lambda = 10.0;
+
+        Network net = new NetworkParser().parse(instancePath);
+        CostCalculator calc = new CostCalculator(lambda);
+
+        new AutoSolver(1000).improve(net, calc);
+        double actualCost = calc.compute(net).total();
+
+        assertEquals(expectedCost, actualCost, EPS_1E3);
+    }
+    @Test
+    @DisplayName("Test instance7.txt")
+    void autoSolverTestInstance7() throws Exception {
+        Path instancePath = Path.of("instances", "instance7.txt");
+        double expectedCost = 5.094;
+        double lambda = 10.0;
+
+        Network net = new NetworkParser().parse(instancePath);
+        CostCalculator calc = new CostCalculator(lambda);
+
+        new AutoSolver(1000).improve(net, calc);
+        double actualCost = calc.compute(net).total();
+
+        assertEquals(expectedCost, actualCost, EPS_1E3);
+    }
+}
